707c6e1205dbe1a49ed7ddedb91741c3c425b44a
Matheus Cabral <suehtamcabrall+git@gmail.com>
2024-09-04 15:47:59 -0300

    Aula 03: Criando a base de dados com JavaScript

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a fenômeno do skate, conquistou o mundo com sua habilidade e carisma. Com apenas 16 anos, ela se tornou a atleta mais jovem a conquistar uma medalha olímpica, no skate street. Sua trajetinha, marcada por manobras inovadoras e um estilo único, a transformou em um ícone para a nova geração de skatistas.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica apurada e determinação. Com diversas medalhas em competições nacionais e internacionais, ela representa o futuro do judô feminino brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com sua performance nas Olimpíadas de Tóquio. Conquistando medalhas de ouro e prata, ela se tornou um símbolo de superação e inspiração para muitos.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
