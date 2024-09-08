1945e6a6781c37624b3444b19a999a3ad77d8dbf
Matheus Cabral <suehtamcabrall+git@gmail.com>
2024-09-08 00:01:55 -0300

    feat: add os dados dos prefeitos

---
+++ assets/js/dados.js
@@ -0,0 +1,184 @@
+let dados = [
+    {
+        titulo: "Adriane Lopes",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/2022_Cerim%C3%B4nia_de_entrega_do_Residencial_Jardim_Canguru%2C_Em_Campo_Grande_-_52184732066.jpg/141px-2022_Cerim%C3%B4nia_de_entrega_do_Residencial_Jardim_Canguru%2C_Em_Campo_Grande_-_52184732066.jpg",
+        descricao: "Adriane Barbosa Nogueira Lopes (Grandes Rios, 29 de junho de 1976) é uma política brasileira, filiada ao Progressistas (PP). É a primeira mulher a ocupar a prefeitura de Campo Grande. Anteriormente, ela atuou como vice-prefeita da cidade de 2017 a 2022, como vice-prefeita do ex-prefeito Marquinhos Trad.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/CENTROOESTE/MS/2045202024/120002167893/2024/90514",
+        tags: "Campo Grande, MS"
+    },
+    {
+        titulo: "Álvaro Dias",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/%C3%81lvaro_Costa_Dias_-_Prefeito_de_Natal_2020.jpg/127px-%C3%81lvaro_Costa_Dias_-_Prefeito_de_Natal_2020.jpg",
+        descricao: "Álvaro Costa Dias, também conhecido como Álvaro Dias, é um médico e político brasileiro, filiado ao Republicanos. É 47.º prefeito de Natal desde 2018.",
+        link: "javascript:void(0)",
+        tags: "Natal, RN"
+    },
+    {
+        titulo: "Arthur Henrique",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/2021-09-29_Cerim%C3%B4nia_alusiva_%C3%A0_Inaugura%C3%A7%C3%A3o_da_UTE_Jaguatirica_II_e_Assinatura_do_contrato_de_concess%C3%A3o_dos_aeroportos_do_bloco_Norte_I_%28cropped%29.jpg/141px-2021-09-29_Cerim%C3%B4nia_alusiva_%C3%A0_Inaugura%C3%A7%C3%A3o_da_UTE_Jaguatirica_II_e_Assinatura_do_contrato_de_concess%C3%A3o_dos_aeroportos_do_bloco_Norte_I_%28cropped%29.jpg",
+        descricao: "Arthur Henrique Brandão Machado nasceu em 19 de agosto de 1981, em Boa Vista (RR). Em 2020 foi eleito prefeito de Boa Vista com 116.792 votos, o equivalente a 85.36% do total.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORTE/RR/2045202024/230002024201/2024/03018",
+        tags: "Boa Vista, RR"
+    },
+    {
+        titulo: "Bruno Reis",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Deputado_Bruno_Reis_na_Lavagem_do_Bonfim_2012.jpg/138px-Deputado_Bruno_Reis_na_Lavagem_do_Bonfim_2012.jpg",
+        descricao: "Bruno Soares Reis (Petrolina, 17 de maio de 1977), mais conhecido como Bruno Reis, é um político brasileiro filiado ao União Brasil (UNIÃO). É o atual prefeito de Salvador, tendo sido eleito nas eleições 2020 ao derrotar Major Denice (PT), com Ana Paula Matos como vice-prefeita e ACM Neto como cabo eleitoral. Filiado ao Democratas (DEM), foi vice-prefeito de Salvador, tendo ACM Neto como prefeito, em 2016. E, entre 2011 e 2017, foi deputado estadual pela Bahia.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/BA/2045202024/50001907549/2024/38490",
+        tags: "Salvador, BA"
+    },
+    {
+        titulo: "Cícero Lucena",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Senador_C%C3%ADcero_Lucena.jpg/111px-Senador_C%C3%ADcero_Lucena.jpg",
+        descricao: "Cícero Lucena Filho GOMM (São José de Piranhas, 5 de agosto de 1957) é um político brasileiro filiado ao Progressistas (PP), atual prefeito de João Pessoa em seu terceiro mandato. Pela Paraíba, foi governador e senador.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/PB/2045202024/150002036085/2024/20516",
+        tags: "João Joao Pessoa, PB"
+    },
+    {
+        titulo: "Cínthia Ribeiro",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Cinthia_Ribeiro_em_maio_de_2019_%281%3B_recorte%29.jpg/117px-Cinthia_Ribeiro_em_maio_de_2019_%281%3B_recorte%29.jpg",
+        descricao: "Fonoaudióloga, a candidata é a atual prefeita de Palmas. Ela foi casada com o senador João Ribeiro, falecido em 2013. Depois da morte do marido, filiou-se ao PSDB e assumiu os negócios da família.",
+        link: "javascript:void(0)",
+        tags: "Palmas, TO"
+    },
+    {
+        titulo: "David Almeida",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Deputado_Estadual_David_Almeida_%28Foto_anterior%29.jpg/129px-Deputado_Estadual_David_Almeida_%28Foto_anterior%29.jpg",
+        descricao: "David Antônio Abisai Pereira de Almeida (Manaus, 8 de fevereiro de 1969), mais conhecido como David Almeida, é um político brasileiro e atual prefeito da cidade de Manaus. Foi deputado estadual por três mandatos consecutivos, sendo eleito pela primeira vez em 2006. O segundo mandato foi em 2010 e o terceiro em 2014. Em dezembro de 2016 foi eleito presidente da Assembleia Legislativa do Estado do Amazonas para o biênio 2017/2018.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORTE/AM/2045202024/40001941186/2024/02550",
+        tags: "Manaus, AM"
+    },
+    {
+        titulo: "Antônio Furlan",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Dr._Furlan_e_Waldez_G%C3%B3es_%28cropped%29.jpg/121px-Dr._Furlan_e_Waldez_G%C3%B3es_%28cropped%29.jpg",
+        descricao: "Concorreu nas eleições de 2020 a prefeito de Macapá, ganhando as eleições no segundo turno contra Josiel Alcolumbre (irmão do senador Davi Alcolumbre e 1º Suplente de seu irmão).",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORTE/AP/2045202024/30002098704/2024/06050",
+        tags: "Macapá Macapa, AP"
+    },
+    {
+        titulo: "Dr. Pessoa",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Dr_Pessoa_-_Tv_Antena_10_%28cropped%29.jpg/134px-Dr_Pessoa_-_Tv_Antena_10_%28cropped%29.jpg",
+        descricao: "José Pessoa Leal (São Pedro do Piauí, 12 de agosto de 1946), conhecido como Dr. Pessoa, é um médico, professor e político brasileiro, filiado ao Partido Renovação Democrática (PRD), e o atual prefeito de Teresina.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/PI/2045202024/180001897285/2024/12190",
+        tags: "Teresina, PI"
+    },
+    {
+        titulo: "Edmilson Rodrigues",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Edmilson_Rodrigues.jpg/130px-Edmilson_Rodrigues.jpg",
+        descricao: "Edmilson Brito Rodrigues é um arquiteto, professor e político brasileiro, filiado ao Partido Socialismo e Liberdade e atual prefeito de Belém, cargo que exerceu anteriormente entre 1997 e 2005. Foi, também, deputado estadual do Pará e deputado federal pelo mesmo estado.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORTE/PA/2045202024/140002109917/2024/04278",
+        tags: "Belém Belem, PA"
+    },
+    {
+        titulo: "Eduardo Braide",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Eduardo_Braide_%28cropped%29.jpg/127px-Eduardo_Braide_%28cropped%29.jpg",
+        descricao: "Eduardo Salim Braide (São Luís, 12 de janeiro de 1976) é um advogado, político brasileiro filiado ao Partido Social Democrático (PSD). É prefeito de São Luís desde 2021.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/MA/2045202024/100002092730/2024/09210",
+        tags: "São Sao Luís Luis, MA"
+    },
+    {
+        titulo: "Eduardo Paes",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Eduardo_Paes%2C_prefeito_em_terceiro_mandato_%28cropped%29.jpg/132px-Eduardo_Paes%2C_prefeito_em_terceiro_mandato_%28cropped%29.jpg",
+        descricao: "Eduardo da Costa Paes GOMA (Rio de Janeiro, 14 de novembro de 1969) é um político brasileiro, filiado ao Partido Social Democrático (PSD). É o atual prefeito do Rio de Janeiro, cargo que ocupou anteriormente de 2009 a 2017.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/SUDESTE/RJ/2045202024/190002135253/2024/60011",
+        tags: "Rio De Janeiro, RJ"
+    },
+    {
+        titulo: "Edvaldo Nogueira",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Edvaldo_Nogueira.png/125px-Edvaldo_Nogueira.png",
+        descricao: "Edvaldo Nogueira Filho (Pão de Açúcar, 25 de janeiro de 1961) é um político brasileiro filiado ao PDT, partido em que se filiou após 39 anos de militância no PCdoB. Atualmente é Prefeito de Aracaju, capital de Sergipe, cargo que já havia ocupado entre 31 de março de 2006 e 31 de dezembro de 2012.",
+        link: "javascript:void(0)",
+        tags: "Aracaju, SE"
+    },
+    {
+        titulo: "Emanuel Pinheiro",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Emanuel_Pinheiro%2C_prefeito_eleito_de_Cuiab%C3%A1_2016.png/140px-Emanuel_Pinheiro%2C_prefeito_eleito_de_Cuiab%C3%A1_2016.png",
+        descricao: "Emanuel Pinheiro (Cuiabá, 12 de abril de 1965) é um advogado e político brasileiro, filiado ao Movimento Democrático Brasileiro (MDB).Foi vereador, deputado estadual e é o prefeito de Cuiabá desde 2017.",
+        link: "javascript:void(0)",
+        tags: "Cuiabá Cuiaba, MT"
+    },
+    {
+        titulo: "Fuad Noman",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/FuadNomanHOJE.jpg/135px-FuadNomanHOJE.jpg",
+        descricao: "Fuad Jorge Noman Filho (Belo Horizonte, 30 de junho de 1947) é um escritor, economista e político brasileiro, filiado ao Partido Social Democrático (PSD). É o atual prefeito de Belo Horizonte, ascendendo ao cargo após a renúncia do titular, Alexandre Kalil.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/SUDESTE/MG/2045202024/130001975610/2024/41238",
+        tags: "Belo Horizonte, MG"
+    },
+    {
+        titulo: "Hildon Chaves",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Dr_Hildon_%28cortado%29.png/117px-Dr_Hildon_%28cortado%29.png",
+        descricao: "Hildon de Lima Chaves nasceu em 25 de maio de 1968, na cidade de Recife-PE, onde residiu até o ano de 1987, quando mudou-se com a família para Curitiba, cidade em que concluiu o ensino médio e, posteriormente, o curso de Direito em Janeiro de 1992.",
+        link: "javascript:void(0)",
+        tags: "Porto Velho, RO"
+    },
+    {
+        titulo: "João Caldas",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/16-12-2015_Vice-presidente_Michel_Temer_recebe_o_deputado_federal_Jo%C3%A3o_Henrique_Caldas_%28JHC%29_PSB-AL._-_Foto-_Anderson_Riedel_%2823771356266%29.jpg/121px-16-12-2015_Vice-presidente_Michel_Temer_recebe_o_deputado_federal_Jo%C3%A3o_Henrique_Caldas_%28JHC%29_PSB-AL._-_Foto-_Anderson_Riedel_%2823771356266%29.jpg",
+        descricao: "João Henrique Holanda Caldas (Maceió, 22 de julho de 1987), mais conhecido como JHC, é um advogado e político brasileiro, filiado ao Partido Liberal (PL). É o atual prefeito de Maceió, eleito com 58,64% dos votos válidos no 2° turno da eleição municipal de 2020, derrotando o candidato Alfredo Gaspar de Mendonça.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/AL/2045202024/20002309875/2024/27855",
+        tags: "Maceió Maceio JHC, AL"
+    },
+    {
+        titulo: "João Campos",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Jo%C3%A3o_Campos_em_24_de_maio_de_2019_%282%3B_recorte%29.jpg/111px-Jo%C3%A3o_Campos_em_24_de_maio_de_2019_%282%3B_recorte%29.jpg",
+        descricao: "João Henrique de Andrade Lima Campos (Recife, 26 de novembro de 1993) é um engenheiro civil e político brasileiro, filiado ao Partido Socialista Brasileiro (PSB), e atual prefeito da cidade do Recife. Ele é o filho do ex-governador de Pernambuco Eduardo Campos e bisneto de Miguel Arraes, uma figura histórica da política pernambucana. João Campos formou-se em Engenharia Civil pela Universidade Federal de Pernambuco (UFPE) em 2016, e desde cedo mostrou interesse pela política, seguindo os passos de sua família. Além de sua formação acadêmica, João Campos é conhecido por seu relacionamento com a deputada federal Tabata Amaral.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/PE/2045202024/170002004102/2024/25313",
+        tags: "Recife, PE"
+    },
+    {
+        titulo: "José Sarto",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Jos%C3%A9_Sarto.jpg/141px-Jos%C3%A9_Sarto.jpg",
+        descricao: "José Sarto Nogueira Moreira (Acopiara, 13 de fevereiro de 1959) é um médico e político brasileiro, sendo desde 2021 prefeito de Fortaleza, capital do estado do Ceará, pelo Partido Democrático Trabalhista (PDT).",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORDESTE/CE/2045202024/60002029261/2024/13897",
+        tags: "Fortaleza, CE"
+    },
+    {
+        titulo: "LORENZO PAZOLINI",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Lorenzo_Pazolini_%28cropped%29.jpg/134px-Lorenzo_Pazolini_%28cropped%29.jpg",
+        descricao: "Lorenzo Silva de Pazolini (Vitória, 20 de maio de 1982) é ex-delegado de polícia, ex-deputado estadual e político brasileiro filiado ao Republicanos. É o atual prefeito de Vitória, assumiu o cargo no dia 1º de janeiro de 2021 após vencer as eleições no segundo turno com 58,50% dos votos válidos, somando 102.466 votos. É atualmente candidato à reeleição da prefeitura de Vitória, sendo líder das pesquisas, com 48,1% das intenções de voto.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/SUDESTE/ES/2045202024/80002366610/2024/57053",
+        tags: "Vitória, ES"
+    },
+    {
+        titulo: "Rafael Greca",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Rafael_Greca_prefeito_2017.png/141px-Rafael_Greca_prefeito_2017.png",
+        descricao: "Rafael Valdomiro Greca de Macedo (Curitiba, 17 de março de 1956) é um economista, engenheiro, urbanista, escritor, poeta, editor, historiador e político brasileiro. Filiado ao Partido Social Democrático (PSD), é o atual prefeito de Curitiba.",
+        link: "javascript:void(0)",
+        tags: "Curitiba, PR"
+    },
+    {
+        titulo: "Ricardo Nunes",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ricardo_Nunes%2C_prefeito_de_S%C3%A3o_Paulo.jpg/117px-Ricardo_Nunes%2C_prefeito_de_S%C3%A3o_Paulo.jpg",
+        descricao: "Ricardo Luís Reis Nunes (São Paulo, 13 de novembro de 1967) é um empresário e político brasileiro, filiado ao Movimento Democrático Brasileiro (MDB), desde os 18 anos. Foi vereador e atualmente é o prefeito de São Paulo e presidente do Conselho de Desenvolvimento da Região Metropolitana de São Paulo.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/SUDESTE/SP/2045202024/250002098117/2024/71072",
+        tags: "São Paulo Sao, SP"
+    },
+    {
+        titulo: "Rogério Cruz",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/35_ANOS-_GUARDA_CIVIL_METROPOLITANA_GANHOU_MAIS_ATRIBUI%C3%87%C3%95ES_%28Rog%C3%A9rio_Cruz%29.png/129px-35_ANOS-_GUARDA_CIVIL_METROPOLITANA_GANHOU_MAIS_ATRIBUI%C3%87%C3%95ES_%28Rog%C3%A9rio_Cruz%29.png",
+        descricao: "Rogério Oliveira da Cruz, mais conhecido como Rogério Cruz (Duque de Caxias, 1 de setembro de 1966) é um radialista, pastor evangélico formado em gestão pública pela UNIP e político brasileiro filiado ao Solidariedade. É o atual prefeito de Goiânia.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/CENTROOESTE/GO/2045202024/90002233443/2024/93734",
+        tags: "Goiânia Goiania, GO"
+    },
+    {
+        titulo: "Sebastião Melo",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/08-12-2021_Lan%C3%A7amento_da_pr%C3%A9-candidatura_de_Simone_Tebet_%C3%A0_Presid%C3%AAncia_da_Rep%C3%BAblica_%28486%29_-_51735868431_%28cropped%29.jpg/109px-08-12-2021_Lan%C3%A7amento_da_pr%C3%A9-candidatura_de_Simone_Tebet_%C3%A0_Presid%C3%AAncia_da_Rep%C3%BAblica_%28486%29_-_51735868431_%28cropped%29.jpg",
+        descricao: "Sebastião de Araújo Melo (Piracanjuba, 24 de julho de 1958) é um advogado e político brasileiro. É o atual prefeito de Porto Alegre.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/SUL/RS/2045202024/210002029933/2024/88013",
+        tags: "Porto Alegre, RS"
+    },
+    {
+        titulo: "Tião Bocalom",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Ti%C3%A3o_Bocalom_%28cropped%29.jpg/131px-Ti%C3%A3o_Bocalom_%28cropped%29.jpg",
+        descricao: "Sebastião Bocalom Rodrigues, conhecido como Tião Bocalom (Bela Vista do Paraíso, 18 de maio de 1953) é um matemático e político brasileiro, filiado ao Partido Liberal (PL). Desde 2021 atua no cargo de prefeito de Rio Branco.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/NORTE/AC/2045202024/10001908807/2024/01392",
+        tags: "Rio Branco, AC"
+    },
+    {
+        titulo: "Topázio Neto",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Prefeito_Top%C3%A1zio_Silveira_Neto_02_%28cropped%29_%28cropped%29.jpg/129px-Prefeito_Top%C3%A1zio_Silveira_Neto_02_%28cropped%29_%28cropped%29.jpg",
+        descricao: "Topázio Silveira Neto (Florianópolis, 16 de abril de 1962) é um empresário e político brasileiro, filiado ao Partido Social Democrático (PSD). É o atual prefeito de Florianópolis. Como empreendedor, foi um dos pioneiros do setor das telecomunicações no estado de Santa Catarina. Em 2020, elegeu-se vice-prefeito de Florianópolis, na chapa de Gean Loureiro, assumindo o comando da Prefeitura definitivamente em 2022, após a renúncia do titular.",
+        link: "https://divulgacandcontas.tse.jus.br/divulga/#/candidato/SUL/SC/2045202024/240002028915/2024/81051",
+        tags: "Florianópolis, SC"
+    },
+]
